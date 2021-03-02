d0cca3ce0c481021d352967ef85e5931e4c67e8c
FCesky <sdftcsdftc@outlook.com>
2021-03-02 10:47:25 +0800

    fix：完善逻辑。新增读取第一片索引数的配置

--- src/drivers/LocalDriver.php
+++ src/drivers/LocalDriver.php
@@ -17,18 +17,21 @@ class LocalDriver implements UploadInterface
     }
 
     //检测切片
-    public function checkFile(FileInfo $f){
+    public function checkFile(FileInfo $f)
+    {
         $identifier = $f->identifier;
         $filePath = $this->config['tmp_dir'] . $identifier; //临时分片文件路径
+        $sliceFirst = $this->config['slice_first'] ?? 1;
         $totalChunks = $f->totalChunks;
+        $loopCount = $sliceFirst ? $totalChunks + 1 : $totalChunks;
         //检查分片是否存在
         $chunkExists = [];
-        for ($index = 1; $index <= $totalChunks; $index++ ) {
+        for ($index = $sliceFirst; $index < $loopCount; $index++) {
             if (file_exists("{$filePath}_{$index}")) {
                 array_push($chunkExists, $index);
             }
         }
-        if (count($chunkExists) == $totalChunks) { 
+        if (count($chunkExists) == $totalChunks) {
             //全部分片存在，则直接合成
             return $this->mergeFile($f);
         } else {
@@ -38,24 +41,18 @@ class LocalDriver implements UploadInterface
         }
     }
     //上传切片
-    public function uploadFile(FileInfo $f){
-        if (!empty($_FILES)) {
-            if (!$in = fopen($_FILES["file"]["tmp_name"], "rb")) {
-                 throw new UploadException('打开临时文件失败');
-            }
-        } else {
-            if (!$in = fopen("php://input", "rb")) {
-                throw new UploadException('打开文件流失败');
-            }
-        }
+    public function uploadFile(FileInfo $f)
+    {
+
+
         if ($f->totalChunks === 1) {
             //如果总共只有1片，则不需要合并，直接将临时文件转存到保存目录下
-            $saveDir = rtrim($this->config['save_dir'],'/')   .DIRECTORY_SEPARATOR .'resources'.DIRECTORY_SEPARATOR. date('Ymd');
+            $saveDir = rtrim($this->config['save_dir'], '/')   . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . date('Ymd');
             if (!is_dir($saveDir)) {
-                mkdir($saveDir,0777,true);
+                mkdir($saveDir, 0777, true);
             }
-            $random = lcg_value();
-            $uploadPath = $saveDir . DIRECTORY_SEPARATOR .$f->identifier.$random.'.'.$f->ext;
+            $random = bin2hex(random_bytes(10));
+            $uploadPath = $saveDir . DIRECTORY_SEPARATOR . $f->identifier . $random . '.' . $f->ext;
             // $res['filepath'] = '/vod/resources/'.date('Ymd') . '/' . $f->identifier.$random.'.'.$f->ext;
             $res['savepath'] = $uploadPath;
             $res['merge'] = false;
@@ -66,48 +63,66 @@ class LocalDriver implements UploadInterface
         }
         $baseDir = dirname($uploadPath);
         if (!is_dir($baseDir)) {
-            mkdir($baseDir,0777,true);
+            mkdir($baseDir, 0777, true);
         }
-        if (!$out = fopen($uploadPath, "wb")) {
-            throw new UploadException('上传的路径没有写入权限');
-        }
-        while ($buff = fread($in, 4096)) {
-            fwrite($out, $buff);
-        }
-        fclose($in);
-        fclose($out);
+        if (!file_exists($uploadPath)) {
 
+            //打开PHP暂存文件
+            if (!empty($_FILES)) {
+                if (!$in = fopen($_FILES["file"]["tmp_name"], "rb")) {
+                    throw new UploadException('打开临时文件失败');
+                }
+            } else {
+                if (!$in = fopen("php://input", "rb")) {
+                    throw new UploadException('打开文件流失败');
+                }
+            }
+            //打开要写入的文件
+            if (!$out = fopen($uploadPath, "wb")) {
+                throw new UploadException('上传的路径没有写入权限');
+            }
+            //执行写入
+            while ($buff = fread($in, 4096)) {
+                fwrite($out, $buff);
+            }
+            fclose($in);
+            fclose($out);
+        }
         $res['code'] = 0;
         return $res;
     }
     //合并切片
-    public function mergeFile(FileInfo $f){
+    public function mergeFile(FileInfo $f)
+    {
         $filePath = $this->config['tmp_dir'] . $f->identifier;
         $totalChunks = $f->totalChunks; //总分片数
         $done = true;
+        $sliceFirst = $this->config['slice_first'] ?? 1;
+        $totalChunks = $f->totalChunks;
+        $loopCount = $sliceFirst ? $totalChunks + 1 : $totalChunks;
         //检查所有分片是否都存在
-        for ($index = 1; $index <= $totalChunks; $index++) {
+        for ($index = $sliceFirst; $index < $loopCount; $index++) {
             if (!file_exists("{$filePath}_{$index}")) {
                 $done = false;
                 break;
             }
         }
         if ($done === false) {
-            throw new UploadException(1007,'分片缺失,无法合并.总分片数:'.$totalChunks.'找到分片数:'.$index);
+            throw new UploadException(1007, '分片缺失,无法合并.总分片数:' . $totalChunks . '找到分片数:' . $index);
         }
         //如果所有文件分片都上传完毕，开始合并
-        $saveDir = rtrim($this->config['save_dir'],'/')  .DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR. date('Ymd');
+        $saveDir = rtrim($this->config['save_dir'], '/')  . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . date('Ymd');
         if (!is_dir($saveDir)) {
-            mkdir($saveDir,0777,true);
+            mkdir($saveDir, 0777, true);
         }
-        $random = lcg_value();
-        $uploadPath = $saveDir . DIRECTORY_SEPARATOR .$f->identifier.$random.'.'.$f->ext;
+        $random = bin2hex(random_bytes(10));
+        $uploadPath = $saveDir . DIRECTORY_SEPARATOR . $f->identifier . $random . '.' . $f->ext;
 
         if (!$out = fopen($uploadPath, "wb")) {
-            throw new UploadException(1006,'upload path is not writable');
+            throw new UploadException(1006, 'upload path is not writable');
         }
-        if (flock($out, LOCK_EX) ) { // 进行排他型锁定
-            for($index = 1; $index <= $totalChunks; $index++ ) {
+        if (flock($out, LOCK_EX)) { // 进行排他型锁定
+            for ($index = 1; $index <= $totalChunks; $index++) {
                 if (!$in = fopen("{$filePath}_{$index}", "rb")) {
                     break;
                 }
